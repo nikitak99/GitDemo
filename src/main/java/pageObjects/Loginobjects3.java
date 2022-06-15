69db2ef01628bb6990686deda120ded7c761e879
nikitak99 <kitturnikita9@gmail.com>
2022-06-15 23:14:37 +0530

    Updated in Demo

---
+++ src/main/java/pageObjects/Loginobjects3.java
@@ -0,0 +1,27 @@
+package pageObjects;
+
+
+public class Loginobjects3 {
+
+	
+	public void email() {
+
+		System.out.println("Enter email id");
+	}
+
+	public void password() {
+
+		System.out.println("Enter password");
+	}
+	
+	public void submit() {
+
+		System.out.println("Click on submit");
+	}
+	
+	public void forgot() {
+
+		System.out.println("Click on forgot password");
+	}
+	
+}
